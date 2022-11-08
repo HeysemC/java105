d65bbb17b2089d82f9465153f116850591c05ba0
Heysen Caglibulanik <heysemmusayasin@gmail.com>
2022-11-08 22:05:31 +0300

    calismalarv6

---
+++ src/ders31_inheritance/CSuv.java
@@ -0,0 +1,38 @@
+package ders31_inheritance;
+
+public class CSuv extends BinekArac{
+    public static void main(String[] args) {
+
+        CSuv suv1=new CSuv();
+        System.out.println(suv1.marka);//Binek araclarin markasi vardir
+        System.out.println(suv1.model);// Binek araclarin modeli olur
+        System.out.println(suv1.motor);//Tum araclar motor kullanir.
+        System.out.println(suv1.plaka);//Tum araclarin plakasi olur.
+        System.out.println(suv1.yakit);//Araclar farkli yakitler kullanir.
+        System.out.println(suv1.yil);//1900
+        suv1.hiz("benzin");//binek araclarin hizi modele gore degisir
+        suv1.teker();//binek araclarin tekeri olur
+
+        CSuv suv2=new CSuv();
+        suv2.marka="Toyata";
+        suv2.model="Rav4";
+        suv2.motor="1.6";
+        suv2.plaka="06 Ank 006";
+        suv2.yakit="Benzin";
+        suv2.yil=2012;
+        suv2.hiz(suv2.yakit);
+
+
+    }
+    public void hiz(String yakit){
+        if (yakit.equalsIgnoreCase("benzin")){
+            System.out.println("Benzinli Suv'lar max 300 km hiz yapar");
+        } else if (yakit.equalsIgnoreCase("Dizel")) {
+            System.out.println("Dizel suv'lar max 260 km yapar");
+        } else if (yakit.equalsIgnoreCase("hibrit")){
+            System.out.println("Hibrit suv'lar max 220 km yapar");
+        }else{
+            System.out.println("yakit belli degil,max km soyleyemem");
+        }
+    }
+}
