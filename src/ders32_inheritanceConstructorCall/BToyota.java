694120b53752467079e9a9ab2e903a43313b7c44
Heysen Caglibulanik <heysemmusayasin@gmail.com>
2022-11-10 23:11:23 +0300

    java105v3

---
+++ src/ders32_inheritanceConstructorCall/BToyota.java
@@ -0,0 +1,16 @@
+package ders32_inheritanceConstructorCall;
+
+public class BToyota extends Araba {
+    BToyota(){
+        System.out.println("Parametresiz Toyota Constructor");
+    }
+
+    BToyota(int pt){
+        System.out.println("Parametreli Toyota Constructor");
+    }
+
+    BToyota(String pt2){
+        super("Mehmet");
+        System.out.println("String parametreli toyota cons.");
+    }
+}
---
+++ src/ders32_inheritanceConstructorCall/DCamry.java
@@ -0,0 +1,23 @@
+package ders32_inheritanceConstructorCall;
+
+public class DCamry extends BToyota{
+
+    DCamry(String pc){
+        super("Deniz");
+        System.out.println("String Parametreli Camry");
+    }
+    DCamry(){}
+
+    public static void main(String[] args) {
+        //Eger kullanilan argumente uygun bir Constructor
+        //parent class'da yoksa CTE olur
+        DCamry camry2=new DCamry("celal");
+
+
+
+        DCamry camry1=new DCamry();
+        //Parametresiz Araba Constructor
+        //Parametresiz Toyota Constructor
+
+    }
+}
